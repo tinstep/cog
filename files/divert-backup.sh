a4e2da1aa24932dc93e17a3aa824132b203bfe4d
Carrick Mortimer <spamme@fastmail.fm>
2018-05-15 11:53:46 +1000

    Create divert-backup.sh

---
+++ files/divert-backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This file is maintained under source control do not edit it.
+
+# This file needs to execute the the backup scripts in the backup folder, however if several upgrades have been done, 
+# This script must find that latest install and execute only the running images.
+
+# This command file will execute the IBM provided backup scripts.
+source /ibm/to/script
